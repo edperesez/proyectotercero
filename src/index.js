9f3f0b61030fdbbc2d2480783ce555a1c9be3b26
edperesez <edperesez7@hotmai.com>
2024-05-23 16:31:23 -0500

    configuracion plantillas, rutas y peticiones

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log("hola mundo estudiantes");
+/* --------------------------------- import --------------------------------- */ 
+import exprees from 'express' 
+import morgan from 'morgan'; 
+
+import {join,dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {engine} from 'express-handlebars'
+/* ----------------------------- initialization ----------------------------- */
+const app =  exprees(); 
+const __dirname =  dirname(fileURLToPath(import.meta.url));
+
+/* --------------------------------- setting -------------------------------- */
+app.set('port' , process.env.PORT || 3000)  
+
+
+app.set('views', join(__dirname, 'views')) 
+app.engine(' .hbs', engine({
+    defaultLayout: 'main', 
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),   
+    extname: '.hbs'
+
+}));
+app.set( 'view engine', ',hbs'); 
+
+/* ------------------------------- Middlewares ------------------------------ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false })) ; 
+app.use(express.json());  
+/* --------------------------------- routers -------------------------------- */
+app.get('/inicio', (red, res) => {
+    res.json({"message":"hola mundo" })
+});
+
+/* ------------------------------ public files ------------------------------ */
+app.use(express.static(join(__dirname, 'public')))
+
+
+/* ------------------------------- run server ------------------------------- */
+app.listen(app.get('port'), () =>  {
+    console.log('server listening on port', app.get('port'));
+});
