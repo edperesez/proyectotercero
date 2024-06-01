3133f882c54b8657f23d07730b92a0f6c5ef90a6
edperesez <edperesez7@hotmai.com>
2024-05-31 21:21:31 -0500

    se creo modulo de editar

--- src/routers/personas.routes.js
+++ src/routers/personas.routes.js
@@ -4,28 +4,50 @@ import pool from '../database.js'
 const router = router();   
 
 
-router.get ('/list', async(req, res) => {
-try { 
-    cons [result] = await pool.query('SELECT * FROM personas');
-    res.render('personas/list', {personas: result})
+router.get ('/list', async(req, res) => { 
+res.render('personas/add') 
+
+}); 
+
+router.post('/add', async (req, res) =>{
+try  { 
+
+const {name,lastname, age} = req.body 
+const newPersona = {
+name, lastname, age  
+
+}  
+
+await pool.query('INSERT INTO personas SET ?', [newPersona]);
+res.redirect('/list');
+
 }  catch (error) {
-    res.status (500).json({ message: error. message}) ; 
+res.status(500). json({ message: error.message });
 
 }
 
 }); 
 
-router.get('/edit/:id', async (req,res) =>{
-    try  {
+router.post('/edit/:id', async (req,res) =>{
+try  {
         const {id} = req. params 
-          const [persona] = await pool.query('SELECT * FROM personas ');
-        
-        res.render('personas/list', {persona: result}) 
+        const {name,lastname,age} = req.body 
+        const editpersona = {name,lastname,age} 
+                            name,
+                            lastname,
+                            age
+
+                        }
+    
+        await pool.query('UPDATE personas SET ? WHERE id = ?', [id]);
+        res.redirect('/list'); 
+}       catch (error){
+        res.status(500).json({ message: error.message});
+}
 
 
-    } catch(error)  {
+    catch (error)  {
         res.status(500).json({ message: error,message});
-    }
-});
+    } 
 
-export default router;
+});
